351cccd4d1a5b73231fe9d660f982c6b480ffd7e
Stewart Lynch <slynch@createchsol.com>
2022-01-20 15:15:15 -0800

    End of lesson 3

--- Wordle/ViewModels/WordleDataModel.swift
+++ Wordle/ViewModels/WordleDataModel.swift
@@ -10,10 +10,13 @@ import SwiftUI
 class WordleDataModel: ObservableObject {
     @Published var guesses: [Guess] = []
     
+    var keyColors = [String : Color]()
+    
     init() {
         newGame()
     }
     
+    // MARK: - Setup
     func newGame() {
         populateDefaults()
     }
@@ -23,6 +26,24 @@ class WordleDataModel: ObservableObject {
         for index in 0...5 {
             guesses.append(Guess(index: index))
         }
+        // reset keyboard colors
+        let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        for char in letters {
+            keyColors[String(char)] = .unused
+        }
+    }
+    
+    // MARK: - Game Play
+    func addToCurrentWord(_ letter: String) {
+        
+    }
+    
+    func enterWord() {
+        
+    }
+    
+    func removeLetterFromCurrentWord() {
+        
     }
     
 }
