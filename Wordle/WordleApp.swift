0129eedecdad98827ae802785f64b02286004f35
Stewart Lynch <slynch@createchsol.com>
2022-01-19 19:07:32 -0800

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Stewart Lynch on 2022-01-19.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
